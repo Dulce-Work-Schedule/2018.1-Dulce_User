ff4dd833dfa3e067beb9c50c649c8aa7d16e559a
Isaque Alves <isaquealvesdl@gmail.com>
2018-06-14 01:28:07 -0300

    HOTFIX Atualizando deploy.sh
    Signed-off-by: Isaque Alves <isaquealvesdl@gmail.com>

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -5,7 +5,7 @@ echo Deploying ...
 cd $TRAVIS_BUILD_DIR/Environments/Production
 sudo docker-compose -f build.yml build
 sudo docker login --username=$DOCKERHUB_USER --password=$DOCKERHUB_PASSWORD
-sudo docker push dulce/user-client:prod
-sudo docker push dulce/user-server:prod
+sudo docker push dulce/login-client:prod
+sudo docker push dulce/login-server:prod
 sudo docker run cdrx/rancher-gitlab-deploy upgrade --rancher-url $RANCHER_URL --rancher-key $RANCHER_ACCESS_KEY --rancher-secret $RANCHER_SECRET_KEY --environment Production --stack Login --service loginclient --finish-upgrade
 sudo docker run cdrx/rancher-gitlab-deploy upgrade --rancher-url $RANCHER_URL --rancher-key $RANCHER_ACCESS_KEY --rancher-secret $RANCHER_SECRET_KEY --environment Production --stack Login --service loginserver --finish-upgrade
